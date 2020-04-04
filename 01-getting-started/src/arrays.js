d96220efde3be495ab7059b87394b4ffa262758a
melveex <mel.vee.artist@gmail.com>
2020-04-04 17:51:03 -0600

    Work for 4 4 2020

---
+++ 01-getting-started/src/arrays.js
@@ -0,0 +1,26 @@
+const arrayFunctions = {
+  addArray: (arr, num) => {
+    arr.push(num);
+    return arr;
+  },
+
+  showArray: (arr) => {
+    let arrDisplay = arr.join();
+    return arrDisplay;
+  },
+
+  totalArray: (arr) => {
+    let sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+    }
+    return sum;
+  },
+
+  clearArray: (arr) => {
+    arr = [];
+    return arr;
+  }
+};
+
+export default arrayFunctions;
