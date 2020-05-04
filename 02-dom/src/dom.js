cc333a4752124e45edc863db24efe4fcf67d7bce
melveex <mel.vee.artist@gmail.com>
2020-05-03 18:12:08 -0600

    DOM so far

---
+++ 02-dom/src/dom.js
@@ -0,0 +1,9 @@
+// What functions do I need?
+
+// add button 
+// remove button
+// show button
+
+document.getElementById("dom").addEventListener("click", function () {
+    console.log("Hi");
+});
